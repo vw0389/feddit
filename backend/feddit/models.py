dc4c0e3f6e39879e9b00f2b08753af8bea7e8692
Victor <dev@vweinert.com>
2022-03-18 15:27:24 -0500

    added models and migrations

--- backend/feddit/models.py
+++ backend/feddit/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    last_login = models.TimeField(default=None)
+    created_at = models.TimeField()
+
+class Posts(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    content = models.TextField(Default=None)
+
+    created_at = models.TimeField()
+
+class Comments(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    created_at = models.TimeField()
+
+class Votes(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    up = models.BooleanField()
+
+    created_at = models.TimeField()
