9d33ee12618138fc157f05f3feea357e7e88ee09
jamestw13 <jamestw13@gmail.com>
2022-03-19 14:42:53 -0500

    pulled in components/pages from previous feddit save

--- frontend/src/components/PopularList/PopularList.js
+++ frontend/src/components/PopularList/PopularList.js
@@ -1,39 +1,15 @@
-import React, { useState } from "react";
-import Post from "../Post";
+import React, { useState } from 'react';
+import Post from '../Post';
+import data from '../../data';
 
 function PopularList() {
-    const [testData, setTestData] = useState([
-        // Template
-        // {
-        //   subfeddit: '',
-        //   poster: '',
-        //   createdAt: '',
-        //   title: '',
-        //   body: '',
-        // },
-        {
-            subfeddit: "jimmyjohnsfans",
-            member: "TheSpicyItalian",
-            createdAt: "02-02-2022",
-            title: "Banana Peppers... Why?",
-            body: "ipsum lorem yada yada",
-            votes: 2,
-        },
-        {
-            subfeddit: "gregorian",
-            member: "confused",
-            createdAt: "01-22-2021",
-            title: "I don't get it",
-            body: "Is this subfeddit about calendars, chants, or what?",
-            votes: 22299,
-        },
-    ]);
-    return (
-        <div className="pop-list">
-            {testData.map((post, index) => (
-                <Post key={index} post={post} />
-            ))}
-        </div>
-    );
+  const [testData, setTestData] = useState(data);
+  return (
+    <div className='pop-list'>
+      {testData.map((post, index) => (
+        <Post key={index} post={post} />
+      ))}
+    </div>
+  );
 }
 export default PopularList;
