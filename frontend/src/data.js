3e98149443b1396b640d177278c654f51cc58cdb
jamestw13 <jamestw13@gmail.com>
2022-03-19 14:31:18 -0500

    refactored test data out of PopularList component

---
+++ frontend/src/data.js
@@ -0,0 +1,26 @@
+export default [
+  // Template
+  // {
+  //   subfeddit: '',
+  //   poster: '',
+  //   createdAt: '',
+  //   title: '',
+  //   body: '',
+  // },
+  {
+    subfeddit: 'jimmyjohnsfans',
+    poster: 'TheSpicyItalian',
+    createdAt: '02-02-2022',
+    title: 'Banana Peppers... Why?',
+    body: 'ipsum lorem yada yada',
+    votes: 2,
+  },
+  {
+    subfeddit: 'gregorian',
+    poster: 'confused',
+    createdAt: '01-22-2021',
+    title: "I don't get it",
+    body: 'Is this subfeddit about calendars, chants, or what?',
+    votes: 22299,
+  },
+];
